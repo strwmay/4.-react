fb7660d576d9b6435a5a2b95143dfb9c5742b445
May <senaidev.may@gmail.com>
2025-02-26 16:11:25 -0300

    modificando a estrutura

--- devlinks/src/App.jsx
+++ devlinks/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import avatar from "./img/gatinha.png";
+import darkPic from "./img/wb.png";
+import lightPic from "./img/bw.png";
 
 import Profile from "./components/profile/profile";
 import Switch from "./components/switch/Switch";
@@ -17,35 +18,38 @@ const App = () => {
 
   return (
     <div id="App" className={isLight && "light"}>
-      <Profile profilepic={avatar}>May</Profile>
+      <div id="container">
+      <Profile profilepic={isLight ? lightPic : darkPic}>May</Profile>
 
       <Switch troca={troca} isLight={isLight}/>
-
+      <div id="links">
       <ul>
         <Links link={"https://strwmay.carrd.co/"}>My Carrd.co</Links>
         <Links link={"https://open.spotify.com/playlist/7Jr20kssSbaXGWARh84hZd?si=9tcAZ5qESYqeb-MzGGVHGg"}>Listen to my Playlist !</Links>
         <Links link={"https://br.pinterest.com/strwmaay/_profile/"}>My Pinterest</Links>
         <Links link={"https://nubank.com.br/cobrar/18yxeq/661fd915-1d87-4dc2-84c6-f68e4a715ca0"}>Buy me a Coffee ~</Links>
       </ul>
+      </div>
 
       <div id="socialLinks">
         <SocialLinks
           link={"https://github.com/strwmay?tab=stars"}
           icon={"logo-github"}
-        />
+          />
         <SocialLinks
           link={"https://www.instagram.com/strwmay/"}
           icon={"logo-instagram"}
-        />
+          />
         <SocialLinks
           link={"https://www.youtube.com/@strwmay"}
           icon={"logo-youtube"}
-        />
+          />
         <SocialLinks
           link={"https://wa.me/5511939400324"}
           icon={"logo-whatsapp"}
-        />
+          />
       </div>
+          </div>
 
       <Footer>@strwmay</Footer>
     </div>
--- devlinks/src/components/switch/Switch.jsx
+++ devlinks/src/components/switch/Switch.jsx
@@ -2,8 +2,8 @@ import styles from "./Switch.module.css";
 
 const Switch = ({ troca, isLight }) => {
   return (
-    <div onClick={troca} className={isLight ? styles.light : ""}>
-      <div id={styles.switch}>
+    <div className={isLight ? styles.light : ""}>
+      <div onClick={troca} id={styles.switch}>
         <button></button>
         <span></span>
       </div>
