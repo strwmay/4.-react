4a12629c4265dcca049f7badcbfbb295ae3203c5
May <senaidev.may@gmail.com>
2025-04-22 10:33:47 -0300

    ajustes app css

--- devsteam/src/components/Header.jsx
+++ devsteam/src/components/Header.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-100 navbar navbar-dark bg-dark">
+      <div id="info" className="d-flex gap-5 w-50 justify-content-between">
+        <div id="logo" role="button" className="d-flex align-items-center">
+          <i className="bi bi-controller fs-1 text-light ms-4"></i>
+          <span className="navbar-brand fw-bold fs-3">DevSteam</span>
+        </div>
 
-export default Header
+        <input type="text" 
+        className="w-100 d-none d-md-block border-0 rounded-1 search px-4 my-2"   
+        placeholder="Search..." />
+      </div>
+
+      <div id="cart">
+        <i className="bi bi-cart4 text-light fs-2 m-4"></i>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
