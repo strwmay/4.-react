eeefacaaea2925f9586397fab39d6d75551c969a
May <senaidev.may@gmail.com>
2025-04-23 08:04:30 -0300

    testando e aprendendo handleAddCarrinho

--- devsteam/src/App.jsx
+++ devsteam/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Promotion from "./components/Promotion";
 
 function App() {
+  const [contador, setContador] = useState(0);
+
+  const handleAddCarrinho = () => {
+    setContador(contador + 1);
+  }
+
   return (
     <>
-      <Header contadorJogos={2} />
+      <Header contadorJogos={contador} />
       <Promotion />
+      {/* <button on onClick={handleAddCarrinho}>+1</button> */}
     </>
   );
 }
